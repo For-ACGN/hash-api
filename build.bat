ebd5cd52d94d875504c70656c482b9420235a290
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-03-16 16:25:19 +0800

    add script about build ror hash utility program.

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
 nasm -f bin -O3 src/x64/hash_api.asm -o bin/x64/hash_api.bin
 nasm -f bin -O3 src/x64/find_api.asm -o bin/x64/find_api.bin
+go build -v -trimpath -ldflags "-s -w" -o bin/hash.exe hash/main.go
