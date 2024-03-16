a8147d928a7c95835b1384846c50c8bdc57d1cd0
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-03-16 14:59:42 +0800

    remove functions about hash, import package rorwk, improve output.

--- hash/main.go
+++ hash/main.go
@@ -1,97 +1,62 @@
 package main
 
 import (
-	"crypto/sha256"
-	"encoding/binary"
 	"encoding/hex"
 	"flag"
 	"fmt"
 	"log"
 	"strings"
+
+	"github.com/For-ACGN/hash-api/rorwk"
 )
 
 var (
 	format   string
-	modName  string
+	dllName  string
 	funcName string
-	key      string
 )
 
 func init() {
 	flag.StringVar(&format, "fmt", "64", "binary format: 32 or 64")
-	flag.StringVar(&modName, "mod", "kernel32.dll", "dll name")
+	flag.StringVar(&dllName, "mod", "kernel32.dll", "module name")
 	flag.StringVar(&funcName, "func", "WinExec", "function name")
-	flag.StringVar(&key, "key", "test_key", "hash key")
 	flag.Parse()
 }
 
 func main() {
-	fmt.Println("module:  ", modName)
+	fmt.Println("module:  ", dllName)
 	fmt.Println("function:", funcName)
-	fmt.Printf("format:   %sbit\n", format)
+	fmt.Printf("format:   %s bit\n", format)
 	fmt.Println()
+	var (
+		numZero string
+		apiHash []byte
+		hashKey []byte
+		err     error
+	)
 	switch format {
 	case "32":
-
+		apiHash, hashKey, err = rorwk.Hash32(dllName, funcName)
+		numZero = "8"
 	case "64":
-		apiHash, hashKey := hash64(modName, funcName, key)
-		fmt.Printf("Hash: 0x%016X\n", apiHash)
-		fmt.Printf("Key:  %s\n", dumpHex(hashKey))
+		apiHash, hashKey, err = rorwk.Hash64(dllName, funcName)
+		numZero = "16"
 	default:
-		log.Fatalln("invalid format")
+		log.Fatalln("invalid format:", format)
 	}
+	if err != nil {
+		log.Fatalln("failed to calculate hash:", err)
+	}
+	fmt.Printf("Hash: 0x%0"+numZero+"X\n", rorwk.BytesToUintptr(apiHash))
+	fmt.Printf("Key:  0x%0"+numZero+"X\n", rorwk.BytesToUintptr(hashKey))
+	fmt.Printf("Hash: %s\n", dumpBytesHex(apiHash))
+	fmt.Printf("Key:  %s\n", dumpBytesHex(hashKey))
 }
 
-func hash64(module, function, key string) (uint64, []byte) {
-	const bits = 8
-	var (
-		seedHash     uint64
-		keyHash      uint64
-		moduleHash   uint64
-		functionHash uint64
-	)
-	hash := sha256.Sum256([]byte(key))
-	hashKey := hash[:8]
-	seedHash = binary.LittleEndian.Uint64(hashKey)
-	for _, b := range hashKey {
-		seedHash = ror64(seedHash, bits+1)
-		seedHash += uint64(b)
-	}
-	keyHash = seedHash
-	for _, b := range hashKey {
-		keyHash = ror64(keyHash, bits+2)
-		keyHash += uint64(b)
-	}
-	moduleHash = seedHash
-	for _, c := range toUnicode(module + "\x00") {
-		moduleHash = ror64(moduleHash, bits+3)
-		moduleHash += uint64(c)
-	}
-	functionHash = seedHash
-	for _, c := range function + "\x00" {
-		functionHash = ror64(functionHash, bits+4)
-		functionHash += uint64(c)
-	}
-	api := seedHash + keyHash + moduleHash + functionHash
-	return api, hashKey
-}
-
-func ror64(value, bits uint64) uint64 {
-	return value>>bits | value<<(64-bits)
-}
-
-func toUnicode(s string) string {
-	var u string
-	for _, c := range strings.ToUpper(s) {
-		u += string(c) + "\x00"
-	}
-	return u
-}
-
-func dumpHex(b []byte) string {
+func dumpBytesHex(b []byte) string {
 	n := len(b)
 	builder := strings.Builder{}
-	builder.Grow(6*n - 2)
+	builder.Grow(len("0xFF, ")*n - len(", "))
 	for i := 0; i < n; i++ {
 		builder.WriteString("0x")
 		v := hex.EncodeToString([]byte{b[i]})
