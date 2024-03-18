00c4f183bc8c815b8f79c697ff3da1efa7380bbb
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-03-18 16:35:41 +0800

    add nasm -f bin -O3 src/x86/hash_api.asm -o bin/x86/hash_api.bin

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
 nasm -f bin -O3 src/x64/hash_api.asm -o bin/x64/hash_api.bin
 nasm -f bin -O3 src/x64/find_api.asm -o bin/x64/find_api.bin
+nasm -f bin -O3 src/x86/hash_api.asm -o bin/x86/hash_api.bin
+nasm -f bin -O3 src/x86/find_api.asm -o bin/x86/find_api.bin
 go build -v -trimpath -ldflags "-s -w" -o bin/hash.exe hash/main.go
