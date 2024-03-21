042f5f724e81f278742c2ade5248559c6140f89b
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-03-21 13:49:36 +0800

    update build script about replace hash_api.asm to api_call.asm.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-nasm -f bin -O3 src/x64/hash_api.asm -o bin/x64/hash_api.bin
 nasm -f bin -O3 src/x64/find_api.asm -o bin/x64/find_api.bin
-nasm -f bin -O3 src/x86/hash_api.asm -o bin/x86/hash_api.bin
+nasm -f bin -O3 src/x64/api_call.asm -o bin/x64/api_call.bin
 nasm -f bin -O3 src/x86/find_api.asm -o bin/x86/find_api.bin
+nasm -f bin -O3 src/x86/api_call.asm -o bin/x86/api_call.bin
 go build -v -trimpath -ldflags "-s -w" -o bin/hash hash/main.go
