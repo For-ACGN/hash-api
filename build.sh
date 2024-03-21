5fe9a894792b51ca3b389de9620bd6ca8a8b2dc5
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-03-21 14:10:18 +0800

    update build script about build hash tool on Windows and Linux.

--- build.sh
+++ build.sh
@@ -2,4 +2,15 @@ nasm -f bin -O3 src/x64/find_api.asm -o bin/x64/find_api.bin
 nasm -f bin -O3 src/x64/api_call.asm -o bin/x64/api_call.bin
 nasm -f bin -O3 src/x86/find_api.asm -o bin/x86/find_api.bin
 nasm -f bin -O3 src/x86/api_call.asm -o bin/x86/api_call.bin
-go build -v -trimpath -ldflags "-s -w" -o bin/hash hash/main.go
+
+export GOOS=linux
+export GOARCH=amd64
+go build -v -trimpath -ldflags "-s -w" -o bin/x64/hash hash/main.go
+export GOARCH=386
+go build -v -trimpath -ldflags "-s -w" -o bin/x86/hash hash/main.go
+
+export GOOS=windows
+export GOARCH=amd64
+go build -v -trimpath -ldflags "-s -w" -o bin/x64/hash.exe hash/main.go
+export GOARCH=386
+go build -v -trimpath -ldflags "-s -w" -o bin/x86/hash.exe hash/main.go
