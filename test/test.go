60946232c8b97a9ac3d1da7c40841ce38e8aa876
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-03-21 13:56:17 +0800

    improve loadShellcode() about copy shellcode, replace hash_api to api_call.

--- test/test.go
+++ test/test.go
@@ -20,20 +20,12 @@ func main() {
 }
 
 func testFindAPI() {
-	shellcode := loadShellcode("find_api")
-	size := uintptr(len(shellcode))
-	mType := uint32(windows.MEM_COMMIT | windows.MEM_RESERVE)
-	mProtect := uint32(windows.PAGE_EXECUTE_READWRITE)
-	scAddr, err := windows.VirtualAlloc(0, size, mType, mProtect)
-	checkError(err)
-	dst := unsafe.Slice((*byte)(unsafe.Pointer(scAddr)), size)
-	copy(dst, shellcode)
-
+	findAPI := loadShellcode("find_api")
 	hash, key := calcHash("kernel32.dll", "ReadProcessMemory")
 	fmt.Printf("hash: 0x%08X\n", hash)
 	fmt.Printf("key:  0x%08X\n", key)
 
-	apiAddr, _, err := syscall.SyscallN(scAddr, hash, key)
+	apiAddr, _, err := syscall.SyscallN(findAPI, hash, key)
 	fmt.Println(err)
 	proc := windows.NewLazySystemDLL("kernel32.dll").NewProc("ReadProcessMemory").Addr()
 	if proc != apiAddr {
@@ -41,14 +33,14 @@ func testFindAPI() {
 	}
 
 	hash, key = calcHash("invalid.dll", "ReadProcessMemory")
-	apiAddr, _, err = syscall.SyscallN(scAddr, hash, key)
+	apiAddr, _, err = syscall.SyscallN(findAPI, hash, key)
 	fmt.Println(err)
 	if apiAddr != 0 {
 		log.Fatalln("unexpected return value:", apiAddr)
 	}
 
 	hash, key = calcHash("kernel32.dll", "Invalid")
-	apiAddr, _, err = syscall.SyscallN(scAddr, hash, key)
+	apiAddr, _, err = syscall.SyscallN(findAPI, hash, key)
 	fmt.Println(err)
 	if apiAddr != 0 {
 		log.Fatalln("unexpected return value:", apiAddr)
@@ -56,35 +48,27 @@ func testFindAPI() {
 }
 
 func testAPICall() {
-	shellcode := loadShellcode("hash_api")
-	size := uintptr(len(shellcode))
-	mType := uint32(windows.MEM_COMMIT | windows.MEM_RESERVE)
-	mProtect := uint32(windows.PAGE_EXECUTE_READWRITE)
-	scAddr, err := windows.VirtualAlloc(0, size, mType, mProtect)
-	checkError(err)
-	dst := unsafe.Slice((*byte)(unsafe.Pointer(scAddr)), size)
-	copy(dst, shellcode)
-
+	apiCall := loadShellcode("api_call")
 	hash, key := calcHash("kernel32.dll", "CreateThread")
 	fmt.Printf("hash: 0x%08X\n", hash)
 	fmt.Printf("key:  0x%08X\n", key)
 	var threadID uint32
 	handle, _, err := syscall.SyscallN(
-		scAddr, hash, key,
-		0, 0, scAddr, 0, windows.CREATE_SUSPENDED,
+		apiCall, hash, key,
+		0, 0, apiCall, 0, windows.CREATE_SUSPENDED,
 		uintptr(unsafe.Pointer(&threadID)),
 	)
 	fmt.Println(err)
 	fmt.Printf("handle: 0x%08X\n", handle)
 
-	err = windows.CloseHandle(windows.Handle(handle))
-	if err != nil {
+	e := windows.CloseHandle(windows.Handle(handle))
+	if e != nil {
 		log.Fatalln("failed to close thread handle:", err)
 	}
 	fmt.Println("thread id:", threadID)
 }
 
-func loadShellcode(name string) []byte {
+func loadShellcode(name string) uintptr {
 	var (
 		shellcode []byte
 		err       error
@@ -98,7 +82,14 @@ func loadShellcode(name string) []byte {
 		log.Fatalln("unsupported architecture:", runtime.GOARCH)
 	}
 	checkError(err)
-	return shellcode
+	size := uintptr(len(shellcode))
+	mType := uint32(windows.MEM_COMMIT | windows.MEM_RESERVE)
+	mProtect := uint32(windows.PAGE_EXECUTE_READWRITE)
+	scAddr, err := windows.VirtualAlloc(0, size, mType, mProtect)
+	checkError(err)
+	dst := unsafe.Slice((*byte)(unsafe.Pointer(scAddr)), size)
+	copy(dst, shellcode)
+	return scAddr
 }
 
 func calcHash(module, function string) (uintptr, uintptr) {
