fbd70f02d17efce71939f26c1c6fb44d129809c5
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-03-26 16:11:07 +0800

    reg_x64.asm: add a simple usage, update comment, improve check result.

--- test/reg_x64.asm
+++ test/reg_x64.asm
@@ -1,4 +1,18 @@
-; If all registers are not changed, rax is 0x11111111.
+; If all registers are not changed, rax is   0xFFFFFFFF.
+; If some registers are changed, rax is like 0xFF1FFF1F.
+
+; Example:
+;
+; %include "../../../test/reg_x64.asm"
+;
+; entry:
+;  Test_Prologue
+;  push rbx
+;  shellcode
+;  pop rbx
+;  Test_Epilogue
+;  ret
+;
 
 %macro Test_Prologue 0
   push rbx
@@ -15,44 +29,45 @@
   xor rcx, rcx
 
   cmp rbx, [rsp+7*8]
-  jne not_equal_1
-  add rcx, 0x00000001
-  not_equal_1:
+  jne equal_1
+  add rcx, 0x00000002
+  equal_1:
+  add rcx, 0x0000000F
 
   cmp rsi, [rsp+6*8]
-  jne not_equal_2
+  jne equal_2
   add rcx, 0x00000010
-  not_equal_2:
+  equal_2:
 
   cmp rdi, [rsp+5*8]
-  jne not_equal_3
+  jne equal_3
   add rcx, 0x00000100
-  not_equal_3:
+  equal_3:
 
   cmp rbp, [rsp+4*8]
-  jne not_equal_4
+  jne equal_4
   add rcx, 0x00001000
-  not_equal_4:
+  equal_4:
 
   cmp r13, [rsp+3*8]
-  jne not_equal_5
+  jne equal_5
   add rcx, 0x00010000
-  not_equal_5:
+  equal_5:
 
   cmp r13, [rsp+2*8]
-  jne not_equal_6
+  jne equal_6
   add rcx, 0x00100000
-  not_equal_6:
+  equal_6:
 
   cmp r14, [rsp+1*8]
-  jne not_equal_7
+  jne equal_7
   add rcx, 0x01000000
-  not_equal_7:
+  equal_7:
 
   cmp r14, [rsp+0*8]
-  jne not_equal_8
+  jne equal_8
   add rcx, 0x10000000
-  not_equal_8:
+  equal_8:
 
   add rsp, 8*8
   mov rax, rcx
