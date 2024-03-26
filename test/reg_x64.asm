9fa7c999960b5bea114067f890c1308196f92d35
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-03-26 16:23:40 +0800

    reg_x64.asm: improve check code, use 0x2 to replace 0x7.

--- test/reg_x64.asm
+++ test/reg_x64.asm
@@ -1,5 +1,5 @@
-; If all registers are not changed, rax is   0xFFFFFFFF.
-; If some registers are changed, rax is like 0xFF1FFF1F.
+; If all registers are not changed, rax is   0x22222222.
+; If some registers are changed, rax is like 0x22122212.
 
 ; Example:
 ;
@@ -29,45 +29,68 @@
   xor rcx, rcx
 
   cmp rbx, [rsp+7*8]
-  jne equal_1
-  add rcx, 0x00000002
+  je equal_1
+  add rcx, 0x00000001
+  jmp end_equal_1
   equal_1:
-  add rcx, 0x0000000F
+  add rcx, 0x00000002
+  end_equal_1:
 
   cmp rsi, [rsp+6*8]
-  jne equal_2
+  je equal_2
   add rcx, 0x00000010
+  jmp end_equal_2
   equal_2:
+  add rcx, 0x00000020
+  end_equal_2:
 
   cmp rdi, [rsp+5*8]
-  jne equal_3
+  je equal_3
   add rcx, 0x00000100
+  jmp end_equal_3
   equal_3:
+  add rcx, 0x00000200
+  end_equal_3:
 
   cmp rbp, [rsp+4*8]
-  jne equal_4
+  je equal_4
   add rcx, 0x00001000
+  jmp end_equal_4
   equal_4:
+  add rcx, 0x00002000
+  end_equal_4:
 
-  cmp r13, [rsp+3*8]
-  jne equal_5
+  cmp r12, [rsp+3*8]
+  je equal_5
   add rcx, 0x00010000
+  jmp end_equal_5
   equal_5:
+  add rcx, 0x00020000
+  end_equal_5:
 
   cmp r13, [rsp+2*8]
-  jne equal_6
+  je equal_6
   add rcx, 0x00100000
+  jmp end_equal_6
   equal_6:
+  add rcx, 0x00200000
+  end_equal_6:
 
   cmp r14, [rsp+1*8]
-  jne equal_7
+  je equal_7
   add rcx, 0x01000000
+  jmp end_equal_7
   equal_7:
+  add rcx, 0x02000000
+  end_equal_7:
 
-  cmp r14, [rsp+0*8]
-  jne equal_8
+  cmp r15, [rsp+0*8]
+  je equal_8
   add rcx, 0x10000000
+  jmp end_equal_8
   equal_8:
+  add rcx, 0x20000000
+  end_equal_8:
 
   add rsp, 8*8
   mov rax, rcx
