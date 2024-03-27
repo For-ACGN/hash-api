9f9bf059ff46647661177c55c016afce6c7b6e0f
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-03-27 14:08:06 +0800

    reg_x86.asm: add Test_Prologue and Test_Epilogue for use easily.

--- test/reg_x86.asm
+++ test/reg_x86.asm
@@ -1,35 +1,48 @@
-; If all registers are not changed, eax is 0x1111.
+; If all registers are not changed, eax is   0x2222.
+; If some registers are changed, eax is like 0x2212.
 
-; Prologue
-push ebx
-push esi
-push edi
-push ebp
+%macro Test_Prologue 0
+  push edi
+  push esi
+  push ebx
+  push ebp
+%endmacro
 
-; Code
+%macro Test_Epilogue 0
+  xor ecx, ecx
 
-; Epilogue
-xor ecx, ecx
+  cmp edi, [esp+3*4]
+  je equal_1
+  add ecx, 0x1000
+  jmp end_equal_1
+  equal_1:
+  add ecx, 0x2000
+  end_equal_1:
 
-cmp ebx, [esp+3*4]
-jne not_equal_1
-add ecx, 0x0001
-not_equal_1:
+  cmp esi, [esp+2*4]
+  je equal_2
+  add ecx, 0x0100
+  jmp end_equal_2
+  equal_2:
+  add ecx, 0x0200
+  end_equal_2:
 
-cmp esi, [esp+2*4]
-jne not_equal_2
-add ecx, 0x0010
-not_equal_2:
+  cmp ebx, [esp+1*4]
+  je equal_3
+  add ecx, 0x0010
+  jmp end_equal_3
+  equal_3:
+  add ecx, 0x0020
+  end_equal_3:
 
-cmp edi, [esp+1*4]
-jne not_equal_3
-add ecx, 0x0100
-not_equal_3:
+  cmp ebp, [esp+0*4]
+  je equal_4
+  add ecx, 0x0001
+  jmp end_equal_4
+  equal_4:
+  add ecx, 0x0002
+  end_equal_4:
 
-cmp ebp, [esp+0*4]
-jne not_equal_4
-add ecx, 0x1000
-not_equal_4:
-
-add esp, 4*4
-mov eax, ecx
+  add esp, 4*4
+  mov eax, ecx
+%endmacro
