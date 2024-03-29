a17dc8517004120758795a653f93616b2ad6dc23
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-03-29 18:00:39 +0800

    asm/x64/api_call.asm: update comment, move include hash_api.

--- example/asm/x64/api_call.asm
+++ example/asm/x64/api_call.asm
@@ -3,76 +3,75 @@
 
 entry:
   ; store context
-  push rbx                      ; store rbx
+  push rbx                              ; store rbx
 
   ; calculate entry address
-  call calc_entry_addr          ; calculate the entry address
-  flag_CEA:                     ; flag for calculate entry address
+  call calc_entry_addr                  ; calculate the entry address
+  flag_CEA:                             ; flag for calculate entry address
 
-  ; clear the direction flag
-  cld
+  cld                                   ; clear the direction flag
 
   ; call "kernel32.dll, CreateThread"
-  sub rsp, 32+5*8               ; reserve stack for arguments
-  mov rcx, 0x9D08BD6B4CE14AE2   ; set function hash
-  mov rdx, 0x702824E783A5AC49   ; set hash key
-  mov r8, 6                     ; set num arguments
-  xor r9, r9                    ; lpThreadAttributes
-  mov qword[rsp+32+0*8], 0      ; dwStackSize
-  lea r10, [rbx+API_WinExec]    ; calculate function address
-  mov [rsp+32+1*8], r10         ; lpStartAddress
-  mov [rsp+32+2*8], rbx         ; lpParameter, set entry address
-  mov qword [rsp+32+3*8], 0     ; dwCreationFlags
-  mov qword [rsp+32+4*8], 0     ; lpThreadId
-  call api_call                 ; call api function
-  add rsp, 32+5*8               ; restore stack for arguments
+  sub rsp, 32+5*8                       ; reserve stack for arguments
+  mov rcx, 0x9D08BD6B4CE14AE2           ; set function hash
+  mov rdx, 0x702824E783A5AC49           ; set hash key
+  mov r8, 6                             ; set num arguments
+  xor r9, r9                            ; lpThreadAttributes
+  mov qword[rsp+32+0*8], 0              ; dwStackSize
+  lea r10, [rbx+API_WinExec]            ; calculate function address
+  mov [rsp+32+1*8], r10                 ; lpStartAddress
+  mov [rsp+32+2*8], rbx                 ; lpParameter, set entry address
+  mov qword [rsp+32+3*8], 0             ; dwCreationFlags
+  mov qword [rsp+32+4*8], 0             ; lpThreadId
+  call api_call                         ; call api function
+  add rsp, 32+5*8                       ; restore stack for arguments
 
   ; call "kernel32.dll, WaitForSingleObject"
-  mov rcx, 0x79A2580C6E2937E5   ; set function hash
-  mov rdx, 0xA280D0DCE28F4296   ; set hash key
-  mov r8, 2                     ; set num arguments
-  mov r9, rax                   ; set thread handle
-  mov r10, 1000                 ; set dwMilliseconds
-  sub rsp, 32+1*8               ; reserve stack
-  mov [rsp+32+0*8], r10         ; dwMilliseconds
-  call api_call                 ; call api function
-  add rsp, 32+1*8               ; restore stack
+  mov rcx, 0x79A2580C6E2937E5           ; set function hash
+  mov rdx, 0xA280D0DCE28F4296           ; set hash key
+  mov r8, 2                             ; set num arguments
+  mov r9, rax                           ; set thread handle
+  mov r10, 1000                         ; set dwMilliseconds
+  sub rsp, 32+1*8                       ; reserve stack
+  mov [rsp+32+0*8], r10                 ; dwMilliseconds
+  call api_call                         ; call api function
+  add rsp, 32+1*8                       ; restore stack
 
   ; restore context
-  pop rbx                       ; restore rbx
-  ret                           ; return to the caller
+  pop rbx                               ; restore rbx
+  ret                                   ; return to the caller
 
 ; calculate shellcode entry address
 calc_entry_addr:
-  pop rax                       ; get return address
-  lea rbx, [rax-flag_CEA]       ; calculate entry address.
-  push rax                      ; push return address
-  ret                           ; return to entry
-
-hash_api:
-  %include "../../../src/x64/api_call.asm"
+  pop rax                               ; get return address
+  lea rbx, [rax-flag_CEA]               ; calculate entry address.
+  push rax                              ; push return address
+  ret                                   ; return to entry
 
 API_WinExec:
-  push rbx                      ; store rbx
-  mov rbx, rcx                  ; read entry address from rcx
+  push rbx                              ; store rbx
+  mov rbx, rcx                          ; read entry address from rcx
 
   ; clear the direction flag
   cld
 
   ; call "kernel32.dll, WinExec"
-  mov rcx, 0xCA2DBA870B222A04   ; set function hash
-  mov rdx, 0xB725F01C80CE0985   ; set hash key
-  mov r8, 2                     ; set num arguments
-  lea r9, [rbx+command]         ; lpCmdLine
-  xor r10, r10                  ; clear r10
-  mov r10b, [rbx+cmd_show]      ; uCmdShow
-  sub rsp, 32+1*8               ; reserve stack
-  mov [rsp+32+0*8], r10         ; uCmdShow
-  call api_call                 ; call api function
-  add rsp, 32+1*8               ; restore stack
+  mov rcx, 0xCA2DBA870B222A04           ; set function hash
+  mov rdx, 0xB725F01C80CE0985           ; set hash key
+  mov r8, 2                             ; set num arguments
+  lea r9, [rbx+command]                 ; lpCmdLine
+  xor r10, r10                          ; clear r10
+  mov r10b, [rbx+cmd_show]              ; uCmdShow
+  sub rsp, 32+1*8                       ; reserve stack
+  mov [rsp+32+0*8], r10                 ; uCmdShow
+  call api_call                         ; call api function
+  add rsp, 32+1*8                       ; restore stack
 
-  pop rbx                       ; restore rbx
-  ret                           ; exit thread
+  pop rbx                               ; restore rbx
+  ret                                   ; exit thread
+
+hash_api:
+  %include "../../../src/x64/api_call.asm"
 
 command:
   db "calc.exe", 0
--- example/asm/x64/find_api.asm
+++ example/asm/x64/find_api.asm
@@ -3,51 +3,51 @@
 
 entry:
   ; store context
-  push rbx                      ; store rbx
+  push rbx                              ; store rbx
 
   ; calculate entry address
-  call calc_entry_addr          ; calculate the entry address
-  flag_CEA:                     ; flag for calculate entry address
+  call calc_entry_addr                  ; calculate the entry address
+  flag_CEA:                             ; flag for calculate entry address
 
   ; clear the direction flag
   cld
 
   ; find "kernel32.dll, WinExec"
-  mov rcx, 0xCA2DBA870B222A04   ; set function hash
-  mov rdx, 0xB725F01C80CE0985   ; set hash key
-  call find_api                 ; try to find api address
-  cmp rax, 0                    ; check target function is found
-  jz not_found                  ;
+  mov rcx, 0xCA2DBA870B222A04           ; set function hash
+  mov rdx, 0xB725F01C80CE0985           ; set hash key
+  call find_api                         ; try to find api address
+  cmp rax, 0                            ; check target function is found
+  jz not_found                          ;
 
   ; ensure stack is 16 bytes aligned
-  push rdi                      ; store rdi
-  mov rdi, rsp                  ; store current stack to rdi
-  and rdi, 0xF                  ; calculate the offset
-  sub rsp, rdi                  ; adjust current stack
+  push rdi                              ; store rdi
+  mov rdi, rsp                          ; store current stack to rdi
+  and rdi, 0xF                          ; calculate the offset
+  sub rsp, rdi                          ; adjust current stack
 
   ; call "kernel32.dll, WinExec"
-  xor rdx, rdx                  ; clear rdx
-  lea rcx, [rbx+command]        ; lpCmdLine
-  mov dl, [rbx+cmd_show]        ; uCmdShow
-  sub rsp, 32                   ; reserve stack
-  call rax                      ; call api function
-  add rsp, 32                   ; restore stack
+  xor rdx, rdx                          ; clear rdx
+  lea rcx, [rbx+command]                ; lpCmdLine
+  mov dl, [rbx+cmd_show]                ; uCmdShow
+  sub rsp, 32                           ; reserve stack
+  call rax                              ; call api function
+  add rsp, 32                           ; restore stack
 
   ; restore aligned stack
-  add rsp, rdi                  ; restore stack from rdi
-  pop rdi                       ; restore rdi
+  add rsp, rdi                          ; restore stack from rdi
+  pop rdi                               ; restore rdi
 
-  not_found:                    ;
+  not_found:                            ;
   ; restore context
-  pop rbx                       ; restore rbx
-  ret                           ; return to the caller
+  pop rbx                               ; restore rbx
+  ret                                   ; return to the caller
 
 ; calculate shellcode entry address
 calc_entry_addr:
-  pop rax                       ; get return address
-  lea rbx, [rax-flag_CEA]       ; calculate entry address.
-  push rax                      ; push return address
-  ret                           ; return to entry
+  pop rax                               ; get return address
+  lea rbx, [rax-flag_CEA]               ; calculate entry address.
+  push rax                              ; push return address
+  ret                                   ; return to entry
 
 hash_api:
   %include "../../../src/x64/find_api.asm"
