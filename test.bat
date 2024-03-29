fdbd9cdf78140909fc6d051dbe21e488638ed4ed
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-03-29 18:24:24 +0800

    add command about build binary to the test scripts.

--- test.bat
+++ test.bat
@@ -1,3 +1,8 @@
+nasm -f bin -O3 src/x64/find_api.asm -o bin/x64/find_api.bin
+nasm -f bin -O3 src/x64/api_call.asm -o bin/x64/api_call.bin
+nasm -f bin -O3 src/x86/find_api.asm -o bin/x86/find_api.bin
+nasm -f bin -O3 src/x86/api_call.asm -o bin/x86/api_call.bin
+
 set GOOS=windows
 set GOARCH=amd64
 go run test/test.go
