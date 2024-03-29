9f105e0d30ad81ac9ac1a986385f3b25a04193b5
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-03-29 18:09:33 +0800

    asm/x64/find_api.asm: improve code about ensure stack is 16 bytes aligned

--- example/asm/x64/find_api.asm
+++ example/asm/x64/find_api.asm
@@ -4,26 +4,21 @@
 entry:
   ; store context
   push rbx                              ; store rbx
+  push rbp                              ; store rbp
+  mov rbp, rsp                          ; create new stack frame
+  and rsp, 0xFFFFFFFFFFFFFFF0           ; ensure stack is 16 bytes aligned
 
   ; calculate entry address
   call calc_entry_addr                  ; calculate the entry address
   flag_CEA:                             ; flag for calculate entry address
 
-  ; clear the direction flag
-  cld
-
   ; find "kernel32.dll, WinExec"
+  cld                                   ; clear the direction flag
   mov rcx, 0xCA2DBA870B222A04           ; set function hash
   mov rdx, 0xB725F01C80CE0985           ; set hash key
   call find_api                         ; try to find api address
   cmp rax, 0                            ; check target function is found
   jz not_found                          ;
-
-  ; ensure stack is 16 bytes aligned
-  push rdi                              ; store rdi
-  mov rdi, rsp                          ; store current stack to rdi
-  and rdi, 0xF                          ; calculate the offset
-  sub rsp, rdi                          ; adjust current stack
 
   ; call "kernel32.dll, WinExec"
   xor rdx, rdx                          ; clear rdx
@@ -33,12 +28,10 @@ entry:
   call rax                              ; call api function
   add rsp, 32                           ; restore stack
 
-  ; restore aligned stack
-  add rsp, rdi                          ; restore stack from rdi
-  pop rdi                               ; restore rdi
-
   not_found:                            ;
   ; restore context
+  mov rsp, rbp                          ; restore stack
+  pop rbp                               ; restore rbp
   pop rbx                               ; restore rbx
   ret                                   ; return to the caller
 
