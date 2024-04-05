4c78efe4a579f6c092b1ea9220e48808477a6417
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-04-05 18:28:26 +0800

    add commands about build not_found and test assembly.

--- test.bat
+++ test.bat
@@ -6,6 +6,10 @@ nasm -f bin -O3 src/x64/api_call.asm -o bin/x64/api_call.bin
 nasm -f bin -O3 src/x86/find_api.asm -o bin/x86/find_api.bin
 nasm -f bin -O3 src/x86/api_call.asm -o bin/x86/api_call.bin
 
+rem build not_found
+nasm -f bin -O3 test/not_found_x64.asm -o test/not_found_x64.bin
+nasm -f bin -O3 test/not_found_x86.asm -o test/not_found_x86.bin
+
 rem build example
 nasm -f bin -O3 example/asm/x64/find_api.asm -o example/asm/x64/find_api.bin
 nasm -f bin -O3 example/asm/x64/api_call.asm -o example/asm/x64/api_call.bin
@@ -13,6 +17,10 @@ nasm -f bin -O3 example/asm/x64/win_exec.asm -o example/asm/x64/win_exec.bin
 nasm -f bin -O3 example/asm/x86/find_api.asm -o example/asm/x86/find_api.bin
 nasm -f bin -O3 example/asm/x86/api_call.asm -o example/asm/x86/api_call.bin
 nasm -f bin -O3 example/asm/x86/win_exec.asm -o example/asm/x86/win_exec.bin
+
+rem test assembly
+"bin/x64/scloader.exe" -sc "test/not_found_x64.bin"
+"bin/x86/scloader.exe" -sc "test/not_found_x86.bin"
 
 rem test example
 "bin/x64/scloader.exe" -sc "example/asm/x64/find_api.bin"
@@ -30,4 +38,8 @@ set GOARCH=386
 go run test/test.go
 go run example/go/main.go
 
+rem clean processes
 taskkill /IM "win32calc.exe" /f
+
+rem clean test binary output
+del test\*.bin
--- test.sh
+++ test.sh
@@ -4,6 +4,10 @@ nasm -f bin -O3 src/x64/api_call.asm -o bin/x64/api_call.bin
 nasm -f bin -O3 src/x86/find_api.asm -o bin/x86/find_api.bin
 nasm -f bin -O3 src/x86/api_call.asm -o bin/x86/api_call.bin
 
+# build not_found
+nasm -f bin -O3 test/not_found_x64.asm -o test/not_found_x64.bin
+nasm -f bin -O3 test/not_found_x86.asm -o test/not_found_x86.bin
+
 # build example
 nasm -f bin -O3 example/asm/x64/find_api.asm -o example/asm/x64/find_api.bin
 nasm -f bin -O3 example/asm/x64/api_call.asm -o example/asm/x64/api_call.bin
@@ -11,6 +15,10 @@ nasm -f bin -O3 example/asm/x64/win_exec.asm -o example/asm/x64/win_exec.bin
 nasm -f bin -O3 example/asm/x86/find_api.asm -o example/asm/x86/find_api.bin
 nasm -f bin -O3 example/asm/x86/api_call.asm -o example/asm/x86/api_call.bin
 nasm -f bin -O3 example/asm/x86/win_exec.asm -o example/asm/x86/win_exec.bin
+
+# test assembly
+"bin/x64/scloader.exe" -sc "test/not_found_x64.bin"
+"bin/x86/scloader.exe" -sc "test/not_found_x86.bin"
 
 # test example
 "bin/x64/scloader.exe" -sc "example/asm/x64/find_api.bin"
@@ -27,3 +35,6 @@ go run example/go/main.go
 export GOARCH=386
 go run test/test.go
 go run example/go/main.go
+
+# clean test binary output
+rm ./test/*.bin
