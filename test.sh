7f34bb556535647145ce25b6bd06fdcd094aee49
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-04-05 18:34:43 +0800

    remove arguments prefix -sc, use file path directly.

--- test.sh
+++ test.sh
@@ -17,16 +17,16 @@ nasm -f bin -O3 example/asm/x86/api_call.asm -o example/asm/x86/api_call.bin
 nasm -f bin -O3 example/asm/x86/win_exec.asm -o example/asm/x86/win_exec.bin
 
 # test assembly
-"bin/x64/scloader.exe" -sc "test/not_found_x64.bin"
-"bin/x86/scloader.exe" -sc "test/not_found_x86.bin"
+"bin/x64/scloader.exe" "test/not_found_x64.bin"
+"bin/x86/scloader.exe" "test/not_found_x86.bin"
 
 # test example
-"bin/x64/scloader.exe" -sc "example/asm/x64/find_api.bin"
-"bin/x64/scloader.exe" -sc "example/asm/x64/api_call.bin"
-"bin/x64/scloader.exe" -sc "example/asm/x64/win_exec.bin"
-"bin/x86/scloader.exe" -sc "example/asm/x86/find_api.bin"
-"bin/x86/scloader.exe" -sc "example/asm/x86/api_call.bin"
-"bin/x86/scloader.exe" -sc "example/asm/x86/win_exec.bin"
+"bin/x64/scloader.exe" "example/asm/x64/find_api.bin"
+"bin/x64/scloader.exe" "example/asm/x64/api_call.bin"
+"bin/x64/scloader.exe" "example/asm/x64/win_exec.bin"
+"bin/x86/scloader.exe" "example/asm/x86/find_api.bin"
+"bin/x86/scloader.exe" "example/asm/x86/api_call.bin"
+"bin/x86/scloader.exe" "example/asm/x86/win_exec.bin"
 
 export GOOS=windows
 export GOARCH=amd64
