cabf741d19eb7d206ac3f962c9dd20460971461e
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-09-11 12:56:35 +0800

    asm/x64/win_exec.asm: update example that use new hash_api module.

--- example/asm/x64/win_exec.asm
+++ example/asm/x64/win_exec.asm
@@ -10,17 +10,27 @@ entry:
   call calc_entry_addr                  ; calculate the entry address
   flag_CEA:                             ; flag for calculate entry address
 
+  ; reserve stack for arguments
+  sub rsp, 2*8
+
   ; call "kernel32.dll, WinExec"
-  mov rcx, 0xCA2DBA870B222A04           ; set function hash
-  mov rdx, 0xB725F01C80CE0985           ; set hash key
-  mov r8, 2                             ; set num arguments
-  lea r9, [rbx+command]                 ; lpCmdLine
+  mov rcx, 0x9BAC085EFA4FDFAE           ; set module name hash
+  mov rdx, 0x221840B185A6EC3D           ; set procedure name hash
+  mov r8,  0xCAF4D6F05577E596           ; set hash key
+  mov r9,  2                            ; set num arguments
+
+  lea r10, [rbx+command]                ; lpCmdLine
+  mov [rsp+0], r10                      ; move argument to stack
   xor r10, r10                          ; clear r10
   mov r10b, [rbx+cmd_show]              ; uCmdShow
-  sub rsp, 32+1*8                       ; reserve stack
-  mov [rsp+32+0*8], r10                 ; uCmdShow
+  mov [rsp+8], r10                      ; move argument to stack
+
+  sub rsp, 32                           ; reserve stack
   call api_call                         ; call api function
-  add rsp, 32+1*8                       ; restore stack
+  add rsp, 32                           ; restore stack
+
+  ; restore stack for arguments
+  add rsp, 2*8
 
   ; restore context
   pop rbx                               ; restore rbx
