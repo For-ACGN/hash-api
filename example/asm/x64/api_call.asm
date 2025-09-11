8d1e1b4767902aa8b16b5302bcdcc336d60586aa
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-09-11 13:27:36 +0800

    asm/x64/api_call.asm: update entry and win_exec about use hash_api.

--- example/asm/x64/api_call.asm
+++ example/asm/x64/api_call.asm
@@ -10,31 +10,44 @@ entry:
   call calc_entry_addr                  ; calculate the entry address
   flag_CEA:                             ; flag for calculate entry address
 
+  ; reserve stack for arguments
+  sub rsp, 6*8
+
   ; call "kernel32.dll, CreateThread"
-  sub rsp, 32+5*8                       ; reserve stack for arguments
-  mov rcx, 0x9D08BD6B4CE14AE2           ; set function hash
-  mov rdx, 0x702824E783A5AC49           ; set hash key
-  mov r8, 6                             ; set num arguments
-  xor r9, r9                            ; lpThreadAttributes
-  mov [rsp+32+0*8], r9                  ; dwStackSize
+  mov rcx, 0xD663A89A079E0973           ; set module name hash
+  mov rdx, 0xB05BCF369A573601           ; set procedure name hash
+  mov r8,  0xE9C877DEE6BFE924           ; set hash key
+  mov r9,  6                            ; set num arguments
+
   lea r10, [rbx+win_exec]               ; calculate function address
-  mov [rsp+32+1*8], r10                 ; lpStartAddress
-  mov [rsp+32+2*8], rbx                 ; lpParameter, set entry address
-  mov [rsp+32+3*8], r9                  ; dwCreationFlags
-  mov [rsp+32+4*8], r9                  ; lpThreadId
+  xor rax, rax                          ; for set zero argument
+  mov [rsp+0*8], rax                    ; lpThreadAttributes
+  mov [rsp+1*8], rax                    ; dwStackSize
+  mov [rsp+2*8], r10                    ; lpStartAddress
+  mov [rsp+3*8], rbx                    ; lpParameter, set entry address
+  mov [rsp+4*8], rax                    ; dwCreationFlags
+  mov [rsp+5*8], rax                    ; lpThreadId
+
+  sub rsp, 32                           ; reserve stack
   call api_call                         ; call api function
-  add rsp, 32+5*8                       ; restore stack for arguments
+  add rsp, 32                           ; restore stack
 
   ; call "kernel32.dll, WaitForSingleObject"
-  mov rcx, 0x79A2580C6E2937E5           ; set function hash
-  mov rdx, 0xA280D0DCE28F4296           ; set hash key
-  mov r8, 2                             ; set num arguments
-  mov r9, rax                           ; set thread handle
+  mov rcx, 0x5C1EE9B9874351C7           ; set module name hash
+  mov rdx, 0x43B95AAF1ACA1E9B           ; set procedure name hash
+  mov r8,  0x9A7B1560D76B5B73           ; set hash key
+  mov r9,  2                            ; set num arguments
+
   mov r10, 1000                         ; set dwMilliseconds
-  sub rsp, 32+1*8                       ; reserve stack
-  mov [rsp+32+0*8], r10                 ; dwMilliseconds
+  mov [rsp+0*8], rax                    ; thread handle
+  mov [rsp+1*8], r10                    ; dwMilliseconds
+
+  sub rsp, 32                           ; reserve stack
   call api_call                         ; call api function
-  add rsp, 32+1*8                       ; restore stack
+  add rsp, 32                           ; restore stack
+
+  ; restore stack for arguments
+  add rsp, 6*8
 
   ; restore context
   pop rbx                               ; restore rbx
@@ -53,17 +66,27 @@ win_exec:
   mov rbx, rcx                          ; read entry address from rcx
   cld                                   ; clear the direction flag
 
+  ; reserve stack for arguments
+  sub rsp, 2*8
+
   ; call "kernel32.dll, WinExec"
-  mov rcx, 0xCA2DBA870B222A04           ; set function hash
-  mov rdx, 0xB725F01C80CE0985           ; set hash key
-  mov r8, 2                             ; set num arguments
-  lea r9, [rbx+command]                 ; lpCmdLine
+  mov rcx, 0x9BAC085EFA4FDFAE           ; set module name hash
+  mov rdx, 0x221840B185A6EC3D           ; set procedure name hash
+  mov r8,  0xCAF4D6F05577E596           ; set hash key
+  mov r9,  2                            ; set num arguments
+
+  lea r10, [rbx+command]                ; lpCmdLine
+  mov [rsp+0], r10                      ; move argument to stack
   xor r10, r10                          ; clear r10
   mov r10b, [rbx+cmd_show]              ; uCmdShow
-  sub rsp, 32+1*8                       ; reserve stack
-  mov [rsp+32+0*8], r10                 ; uCmdShow
+  mov [rsp+8], r10                      ; move argument to stack
+
+  sub rsp, 32                           ; reserve stack
   call api_call                         ; call api function
-  add rsp, 32+1*8                       ; restore stack
+  add rsp, 32                           ; restore stack
+
+  ; restore stack for arguments
+  add rsp, 2*8
 
   ; restore context
   pop rbx                               ; restore rbx
