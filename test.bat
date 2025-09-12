6f0c22bbd7b5aa24f10650be52145c0be03fe32f
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-09-12 14:44:02 +0800

    improve the echo content, remove the comment in script.

--- test.bat
+++ test.bat
@@ -1,45 +1,53 @@
 @echo off
 
-rem build hash_api
+echo build hash_api
 nasm -f bin -O3 src/x64/find_api.asm -o bin/x64/find_api.bin
 nasm -f bin -O3 src/x64/api_call.asm -o bin/x64/api_call.bin
 nasm -f bin -O3 src/x86/find_api.asm -o bin/x86/find_api.bin
 nasm -f bin -O3 src/x86/api_call.asm -o bin/x86/api_call.bin
 
-rem build not_found
+echo build not_found
 nasm -f bin -O3 test/not_found_x64.asm -o test/not_found_x64.bin
 nasm -f bin -O3 test/not_found_x86.asm -o test/not_found_x86.bin
 
-rem build example
+echo build assembly example
 nasm -f bin -O3 example/asm/x64/find_api.asm -o example/asm/x64/find_api.bin
 nasm -f bin -O3 example/asm/x64/api_call.asm -o example/asm/x64/api_call.bin
 nasm -f bin -O3 example/asm/x64/win_exec.asm -o example/asm/x64/win_exec.bin
 nasm -f bin -O3 example/asm/x86/find_api.asm -o example/asm/x86/find_api.bin
 nasm -f bin -O3 example/asm/x86/api_call.asm -o example/asm/x86/api_call.bin
 nasm -f bin -O3 example/asm/x86/win_exec.asm -o example/asm/x86/win_exec.bin
+echo.
 
-rem test assembly
+echo test not_found
 "bin/x64/scloader.exe" "test/not_found_x64.bin"
 "bin/x86/scloader.exe" "test/not_found_x86.bin"
+echo.
 
-rem test example
+echo test assembly example
 "bin/x64/scloader.exe" "example/asm/x64/find_api.bin"
 "bin/x64/scloader.exe" "example/asm/x64/api_call.bin"
 "bin/x64/scloader.exe" "example/asm/x64/win_exec.bin"
 "bin/x86/scloader.exe" "example/asm/x86/find_api.bin"
 "bin/x86/scloader.exe" "example/asm/x86/api_call.bin"
 "bin/x86/scloader.exe" "example/asm/x86/win_exec.bin"
+echo.
 
+echo test Golang example on x64
 set GOOS=windows
 set GOARCH=amd64
 go run test/test.go
 go run example/go/main.go
+echo.
+
+echo test Golang example on x86
 set GOARCH=386
 go run test/test.go
 go run example/go/main.go
+echo.
 
-rem clean processes
+echo clean processes
 taskkill /IM "win32calc.exe" /f
 
-rem clean test binary output
+echo clean test binary output files
 del test\*.bin
--- test.sh
+++ test.sh
@@ -1,40 +1,48 @@
-# build hash_api
+echo build hash_api
 nasm -f bin -O3 src/x64/find_api.asm -o bin/x64/find_api.bin
 nasm -f bin -O3 src/x64/api_call.asm -o bin/x64/api_call.bin
 nasm -f bin -O3 src/x86/find_api.asm -o bin/x86/find_api.bin
 nasm -f bin -O3 src/x86/api_call.asm -o bin/x86/api_call.bin
 
-# build not_found
+echo build not_found
 nasm -f bin -O3 test/not_found_x64.asm -o test/not_found_x64.bin
 nasm -f bin -O3 test/not_found_x86.asm -o test/not_found_x86.bin
 
-# build example
+echo build assembly example
 nasm -f bin -O3 example/asm/x64/find_api.asm -o example/asm/x64/find_api.bin
 nasm -f bin -O3 example/asm/x64/api_call.asm -o example/asm/x64/api_call.bin
 nasm -f bin -O3 example/asm/x64/win_exec.asm -o example/asm/x64/win_exec.bin
 nasm -f bin -O3 example/asm/x86/find_api.asm -o example/asm/x86/find_api.bin
 nasm -f bin -O3 example/asm/x86/api_call.asm -o example/asm/x86/api_call.bin
 nasm -f bin -O3 example/asm/x86/win_exec.asm -o example/asm/x86/win_exec.bin
+echo
 
-# test assembly
+echo test not_found
 "bin/x64/scloader.exe" "test/not_found_x64.bin"
 "bin/x86/scloader.exe" "test/not_found_x86.bin"
+echo
 
-# test example
+echo test assembly example
 "bin/x64/scloader.exe" "example/asm/x64/find_api.bin"
 "bin/x64/scloader.exe" "example/asm/x64/api_call.bin"
 "bin/x64/scloader.exe" "example/asm/x64/win_exec.bin"
 "bin/x86/scloader.exe" "example/asm/x86/find_api.bin"
 "bin/x86/scloader.exe" "example/asm/x86/api_call.bin"
 "bin/x86/scloader.exe" "example/asm/x86/win_exec.bin"
+echo
 
+echo test Golang example on x64
 export GOOS=windows
 export GOARCH=amd64
 go run test/test.go
 go run example/go/main.go
+echo
+
+echo test Golang example on x86
 export GOARCH=386
 go run test/test.go
 go run example/go/main.go
+echo
 
-# clean test binary output
+echo clean test binary output files
 rm ./test/*.bin
