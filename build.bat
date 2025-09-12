7fd44e677f988720aa56581af00ff45828e60c76
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-09-12 14:43:09 +0800

    replace comment to echo for output the build process.

--- build.bat
+++ build.bat
@@ -1,12 +1,12 @@
 @echo off
 
-rem build hash_api
+echo build hash_api
 nasm -f bin -O3 src/x64/find_api.asm -o bin/x64/find_api.bin
 nasm -f bin -O3 src/x64/api_call.asm -o bin/x64/api_call.bin
 nasm -f bin -O3 src/x86/find_api.asm -o bin/x86/find_api.bin
 nasm -f bin -O3 src/x86/api_call.asm -o bin/x86/api_call.bin
 
-rem build example
+echo build example
 nasm -f bin -O3 example/asm/x64/find_api.asm -o example/asm/x64/find_api.bin
 nasm -f bin -O3 example/asm/x64/api_call.asm -o example/asm/x64/api_call.bin
 nasm -f bin -O3 example/asm/x64/win_exec.asm -o example/asm/x64/win_exec.bin
@@ -14,18 +14,16 @@ nasm -f bin -O3 example/asm/x86/find_api.asm -o example/asm/x86/find_api.bin
 nasm -f bin -O3 example/asm/x86/api_call.asm -o example/asm/x86/api_call.bin
 nasm -f bin -O3 example/asm/x86/win_exec.asm -o example/asm/x86/win_exec.bin
 
-rem build develop tool
+echo build development tool
 set GOOS=windows
 set GOARCH=amd64
 go build -v -trimpath -ldflags "-s -w" -o bin/x64/hash.exe cmd/hash/main.go
 go build -v -trimpath -ldflags "-s -w" -o bin/x64/hexbin.exe cmd/hexbin/main.go
 go build -v -trimpath -ldflags "-s -w" -o bin/x64/scloader.exe cmd/scloader/main.go
-go build -v -trimpath -ldflags "-s -w" -o example/go/go_amd64.exe example/go/main.go
 set GOARCH=386
 go build -v -trimpath -ldflags "-s -w" -o bin/x86/hash.exe cmd/hash/main.go
 go build -v -trimpath -ldflags "-s -w" -o bin/x86/hexbin.exe cmd/hexbin/main.go
 go build -v -trimpath -ldflags "-s -w" -o bin/x86/scloader.exe cmd/scloader/main.go
-go build -v -trimpath -ldflags "-s -w" -o example/go/go_386.exe example/go/main.go
 
 set GOOS=linux
 set GOARCH=amd64
--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-# build hash_api
+echo build hash_api
 nasm -f bin -O3 src/x64/find_api.asm -o bin/x64/find_api.bin
 nasm -f bin -O3 src/x64/api_call.asm -o bin/x64/api_call.bin
 nasm -f bin -O3 src/x86/find_api.asm -o bin/x86/find_api.bin
 nasm -f bin -O3 src/x86/api_call.asm -o bin/x86/api_call.bin
 
-# build example
+echo build example
 nasm -f bin -O3 example/asm/x64/find_api.asm -o example/asm/x64/find_api.bin
 nasm -f bin -O3 example/asm/x64/api_call.asm -o example/asm/x64/api_call.bin
 nasm -f bin -O3 example/asm/x64/win_exec.asm -o example/asm/x64/win_exec.bin
@@ -12,7 +12,7 @@ nasm -f bin -O3 example/asm/x86/find_api.asm -o example/asm/x86/find_api.bin
 nasm -f bin -O3 example/asm/x86/api_call.asm -o example/asm/x86/api_call.bin
 nasm -f bin -O3 example/asm/x86/win_exec.asm -o example/asm/x86/win_exec.bin
 
-# build develop tool
+echo build development tool
 export GOOS=linux
 export GOARCH=amd64
 go build -v -trimpath -ldflags "-s -w" -o bin/x64/hash cmd/hash/main.go
@@ -26,9 +26,7 @@ export GOARCH=amd64
 go build -v -trimpath -ldflags "-s -w" -o bin/x64/hash.exe cmd/hash/main.go
 go build -v -trimpath -ldflags "-s -w" -o bin/x64/hexbin.exe cmd/hexbin/main.go
 go build -v -trimpath -ldflags "-s -w" -o bin/x64/scloader.exe cmd/scloader/main.go
-go build -v -trimpath -ldflags "-s -w" -o example/go/go_amd64.exe example/go/main.go
 export GOARCH=386
 go build -v -trimpath -ldflags "-s -w" -o bin/x86/hash.exe cmd/hash/main.go
 go build -v -trimpath -ldflags "-s -w" -o bin/x86/hexbin.exe cmd/hexbin/main.go
 go build -v -trimpath -ldflags "-s -w" -o bin/x86/scloader.exe cmd/scloader/main.go
-go build -v -trimpath -ldflags "-s -w" -o example/go/go_386.exe example/go/main.go
